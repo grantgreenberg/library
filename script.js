9f8017d6e0d91b31179ebd64f3acebad60a70273
Grant Greenberg <19313835+grantgreenberg@users.noreply.github.com>
2023-12-15 16:04:09 -0700

    feat: update js to use class notation.

--- script.js
+++ script.js
@@ -1,139 +1,147 @@
-let myLibrary = [];
+class Book {
+    constructor(title, author, read = false) {
+        this.title = title;
+        this.author = author;
+        this.read = read;
+    }
 
-function Book(title, author, read = false) {
-  // the constructor...
-  this.title = title;
-  this.author = author;
-  this.read = read;
-};
+    toggleRead() {
+        this.read = !this.read;
+    }
+}
 
-function addBookToLibrary() {
-  // do stuff here
-    const libraryDiv = document.querySelector('.library');
+class Library {
+    constructor() {
+        this.books = [];
+    }
 
-    libraryDiv.textContent = '';
+    addBook(book) {
+        this.books.push(book);
+    }
 
-    myLibrary.forEach(obj => {
+    removeBookByTitle(title) {
+        this.books = this.books.filter(book => book.title !== title);
+    }
 
-        let newBookDiv = document.createElement('div');
-        newBookDiv.classList.add('book-card');
+    displayBooks(libraryDiv) {
+        libraryDiv.textContent = '';
 
-        let bookTitle = document.createElement('p');
-        let bookAuthor = document.createElement('p');
-        let bookRead = document.createElement('p');
-        let readButton = document.createElement('button');
-        let removeButton = document.createElement('button');
+        this.books.forEach(book => {
+            const newBookDiv = document.createElement('div');
+            newBookDiv.classList.add('book-card');
 
-        bookTitle.classList.add('book-title');
+            const elements = [{
+                    tag: 'p',
+                    class: 'book-title',
+                    text: `Title: ${book.title}`
+                },
+                {
+                    tag: 'p',
+                    text: `Author: ${book.author}`
+                },
+                {
+                    tag: 'p',
+                    class: book.read ? 'read-yes' : 'read-no',
+                    text: `Read: ${book.read ? 'Yes' : 'No'}`,
+                    statusClass: 'read-status'
+                },
+                {
+                    tag: 'button',
+                    class: 'toggle-read',
+                    text: 'Toggle Read'
+                },
+                {
+                    tag: 'button',
+                    class: 'remove-book',
+                    text: 'Remove Book'
+                },
+            ];
 
-        readButton.classList.add('toggle-read');
-        readButton.textContent = 'Toggle Read';
+            elements.forEach(({
+                tag,
+                class: className,
+                text,
+                statusClass
+            }) => {
+                const element = document.createElement(tag);
+                element.classList.add(className);
+                element.textContent = text;
 
-        removeButton.classList.add('remove-book');
-        removeButton.textContent = 'Remove Book';
+                if (statusClass) {
+                    element.classList.add(statusClass);
+                }
 
-        bookTitle.textContent = `Title: ${obj.title}`;
-        bookAuthor.textContent = `Author: ${obj.author}`;
-        
-        if (obj.read == 1) {
-            bookRead.textContent = 'Read: Yes';
-            bookRead.classList.add('read-yes');
-            bookRead.classList.add('read-status');
-        }
-        else {
-            bookRead.textContent = 'Read: No';
-            bookRead.classList.add('read-no');
-            bookRead.classList.add('read-status');
-        }
+                newBookDiv.appendChild(element);
+            });
 
-        newBookDiv.appendChild(bookTitle);
-        newBookDiv.appendChild(bookAuthor);
-        newBookDiv.appendChild(bookRead);
-        newBookDiv.appendChild(readButton);
-        newBookDiv.appendChild(removeButton);
+            libraryDiv.appendChild(newBookDiv);
+        });
+    }
+}
 
-        libraryDiv.appendChild(newBookDiv);
-
-    });
-
-};
-
+const myLibrary = new Library();
+const libraryDiv = document.querySelector('.library');
 const dialogButton = document.querySelector('.add-book-button');
 const dialogForm = document.querySelector('.dialog');
 const submitForm = document.querySelector('.submit-form');
 const cancelForm = document.querySelector('.cancel-form');
 
+function refreshLibrary() {
+    myLibrary.displayBooks(libraryDiv);
+}
+
 dialogButton.addEventListener('click', () => {
-
     dialogForm.showModal();
-
 });
 
 submitForm.addEventListener('click', (event) => {
-
     event.preventDefault();
 
-    let formTitle = document.querySelector('#title');
-    let formAuthor = document.querySelector('#author');
-    let formRead = document.querySelector('#read');
+    const formTitle = document.querySelector('#title');
+    const formAuthor = document.querySelector('#author');
+    const formRead = document.querySelector('#read');
 
     if (isValidForm(formTitle) && isValidForm(formAuthor) && isValidForm(formRead)) {
-        let newBook = new Book(formTitle.value, formAuthor.value, formRead.checked);
-        myLibrary.push(newBook);
-        addBookToLibrary();
+        const newBook = new Book(formTitle.value, formAuthor.value, formRead.checked);
+        myLibrary.addBook(newBook);
+        refreshLibrary();
         dialogForm.close();
     } else {
-        alert('Invalid inputs. Please check your title and author for special characters. The Read input should be either 1 or 0.');
+        alert('Invalid inputs. Please check your title and author for special characters.');
     }
-
 });
 
+cancelForm.addEventListener('click', (event) => {
+    event.preventDefault();
+    refreshLibrary();
+    dialogForm.close();
+});
+
+libraryDiv.addEventListener('click', (event) => {
+    const target = event.target;
+
+    if (target.classList.contains('toggle-read')) {
+        const bookCard = target.closest('.book-card');
+        const readStatus = bookCard.querySelector('.read-status');
+        const bookTitle = bookCard.querySelector('.book-title').textContent.slice(7);
+
+        const book = myLibrary.books.find(book => book.title === bookTitle);
+
+        if (book) {
+            book.toggleRead();
+            refreshLibrary();
+        }
+    }
+
+    if (target.classList.contains('remove-book')) {
+        const bookCard = target.closest('.book-card');
+        const bookTitle = bookCard.querySelector('.book-title').textContent.slice(7);
+        myLibrary.removeBookByTitle(bookTitle);
+        refreshLibrary();
+    }
+});
 
 function isValidForm(inputElement) {
     const pattern = new RegExp(inputElement.pattern);
     return pattern.test(inputElement.value);
 }
-
-cancelForm.addEventListener('click', (event) => {
-
-    event.preventDefault();
-
-    addBookToLibrary();
-
-    dialogForm.close();
-
-});
-
-
-document.querySelector('.library').addEventListener('click', (event) => {
-
-    const target = event.target;
-
-    if (target.classList.contains('toggle-read')) {
-
-        const bookCard = target.closest('.book-card');
-        const readStatus = bookCard.querySelector('.read-status');
-
-        if (readStatus.classList.contains('read-yes')) {
-
-            readStatus.textContent = 'Read: No';
-            readStatus.classList.replace('read-yes', 'read-no');
-
-        } else {
-
-            readStatus.textContent = 'Read: Yes';
-            readStatus.classList.replace('read-no', 'read-yes');
-
-        }
-    }
-
-    if (target.classList.contains('remove-book')) {
-
-        const bookCard = target.closest('.book-card');
-        const bookTitle = bookCard.querySelector('.book-title').textContent.slice(7);
-
-        myLibrary = myLibrary.filter(book => book.title !== bookTitle);
-
-        addBookToLibrary();
-    }
-});
